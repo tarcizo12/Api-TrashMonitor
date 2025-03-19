b2f7783b03a3879359fd2eccedd43a383653bef5
tarcizo12 <tarcizon12@gmail.com>
2025-03-19 18:58:52 -0300

    Implementa conexao com MQTT

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./config/database");
-const dataRoutes = require("./routes/data.routes");
-
-// Configurar variáveis de ambiente
-dotenv.config();
-
-// Conectar ao banco
-connectDB();
+const { client, getLastMessages } = require("./config/mqtt");
 
 const app = express();
+const PORT = 4000;
+
+// Usar JSON no body
 app.use(express.json());
 
-// Definir rotas
-app.use("/api", dataRoutes);
+// Endpoint para retornar as últimas 5 mensagens do broker MQTT
+app.get("/mqtt/messages", (req, res) => {
+  // Obter as últimas mensagens
+  const messages = getLastMessages();
+  res.json({ messages });
+});
 
-module.exports = app;
+// Endpoint de teste para verificar se a API está funcionando corretamente
+app.get("/teste", (req, res) => {
+  res.json({ message: "API está funcionando corretamente!" });
+});
+
+// Função para iniciar a conexão com o MQTT e o servidor Express
+const startServer = () => {
+  // Iniciar o servidor Express
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+};
+
+// Chamar a função para iniciar o servidor e a conexão MQTT
+startServer();
--- src/config/mqtt.js
+++ src/config/mqtt.js
@@ -1,16 +1,55 @@
-const mqtt = require("mqtt");
+var mqtt = require('mqtt');
+const dotenv = require("dotenv");
+dotenv.config();
 
-const brokerUrl = "mqtt://seu-broker.com";
-const client = mqtt.connect(brokerUrl);
+var messages = []; // Armazenar as últimas 5 mensagens recebidas
 
-client.on("connect", () => {
-  console.log("Conectado ao broker MQTT!");
-  client.subscribe("topico/hardware");
+// Opções de conexão com base nas suas credenciais
+var options = {
+    host: process.env.MQTT_BROKER_URL, 
+    port: process.env.MQTT_PORT,        
+    protocol: 'mqtts',                  
+    username: process.env.MQTT_USERNAME, 
+    password: process.env.MQTT_PASSWORD  
+};
+
+console.log("opcoes, ", options)
+
+
+const client = mqtt.connect(options);
+
+
+client.on('connect', function () {
+    console.log('Conectado ao broker MQTT');
 });
 
-client.on("message", (topic, message) => {
-  console.log(`Mensagem recebida: ${message.toString()}`);
-  // Aqui você pode salvar no MongoDB ou processar de outra forma
+client.on('error', function (error) {
+    console.log('Erro na conexão MQTT:', error);
 });
 
-module.exports = client;
+
+client.on('message', function (topic, message) {
+    console.log('Mensagem recebida no tópico', topic, ':', message.toString());
+
+    // Adicionar a mensagem ao array de mensagens (limitar a 5 mensagens)
+    if (messages.length >= 5) {
+        messages.shift(); // Remove a mensagem mais antiga
+    }
+    messages.push(message.toString());
+});
+
+// Inscrever-se no tópico 'MonitoramentoLixo'
+client.subscribe('MonitoramentoLixo', function (err) {
+    if (!err) {
+        console.log('Inscrito no tópico: MonitoramentoLixo');
+    } else {
+        //console.log('Erro ao se inscrever no tópico:', err);
+    }
+});
+
+
+const  getLastMessages = () => {
+    return messages;
+}
+
+module.exports = { client, getLastMessages };
